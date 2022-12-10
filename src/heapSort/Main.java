76b0da8a282df0682b629e809e9bb8f1ba624c4a
walysonrodrigo <walysonrodrigo@gmail.com>
2022-12-10 15:01:45 -0300

    criando vetor aleatorio

--- src/heapSort/Main.java
+++ src/heapSort/Main.java
@@ -1,5 +1,18 @@
 package heapSort;
 
 public class Main {
+	public static void main(String [] args) {
+		int [] vetor = new int[10];
+		
+		for(int i = 0; i < vetor.length; i++) {
+			vetor[i] = (int) Math.floor(Math.random() * vetor.length);
+		}
+		
+		System.out.println("Desordenado");
+		for(int i = 0; i < vetor.length; i++){
+			System.out.print(vetor[i] + " ");
+		}
+		
+	}
 
 }
